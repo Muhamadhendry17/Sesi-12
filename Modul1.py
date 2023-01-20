5cf69cdede5885bfeade35a2ad2b03049bac2cb0
Muhamadhendry17 <109840559+Muhamadhendry17@users.noreply.github.com>
2023-01-20 21:25:43 +0700

    Add files via upload

---
+++ Modul1.py
@@ -0,0 +1,11 @@
+kapital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def kapitalCount(text):
+    kap = 0
+
+    for char in text:
+        if char in kapital:
+            kap += 1
+
+    return kap
---
+++ modul2.py
@@ -0,0 +1,6 @@
+import Modul1
+
+text = input("Masukan text: APakaH SayA? ")
+HurufGede = Modul1.kapitalCount(text)
+print(text)
+print("Jumlah huruf besar: ", HurufGede)
